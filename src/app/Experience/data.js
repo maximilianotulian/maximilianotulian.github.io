07287251813ec4799f5acf48feb494e0000ec3a8
Maximiliano Tulian <maxtulian93@gmail.com>
2021-05-07 15:23:08 -0300

    Update experience data

--- src/app/Experience/data.js
+++ src/app/Experience/data.js
@@ -2,7 +2,7 @@ const experience = [
   {
     agile: ['Scrum'],
     date: 'May 2021 to present',
-    description: '',
+    description: [],
     subtitle: 'Rosario',
     technologies: ['React', 'Redux', 'Bootstrap', 'Sass', 'BEM'],
     title: 'Senior Frontend Developer at Grupo Giga',
@@ -10,7 +10,11 @@ const experience = [
   {
     agile: ['Kanban'],
     date: 'January 2021 to May 2021',
-    description: 'Working mainly in react and related technologies. Create reusable components for different breakpoints. Asist in technical decisions related to tech task and future tasks',
+    description: [
+      'Working mainly in react and related technologies.',
+      'Creating reusable components for different breakpoints.',
+      'Asist in technical decisions related to tech task and future tasks.',
+    ],
     subtitle: 'United States',
     technologies: ['React', 'Redux', 'Bootstrap', 'Sass', 'BEM'],
     title: 'Principal Software Engineer at CMOList',
@@ -18,7 +22,11 @@ const experience = [
   {
     agile: ['Scrum'],
     date: 'April to August 2020',
-    description: 'Creating 3d models with threejs that enables the product creation in real time. Applying previous experience to build reusable components. Estimate, analize, build and showcase long term features (more than one month of development)',
+    description: [
+      'Creating 3d models with threejs that enables the product creation in real time.',
+      'Applying previous experience to build reusable components.',
+      'Estimate, analize, build and showcase long term features (more than one month of development)',
+    ],
     subtitle: 'San Francisco',
     technologies: ['React', 'Redux', 'Sass', 'BEM', 'Python', 'Django', 'Webpack', 'Npm', 'Storybook'],
     title: 'Software Engineer at Ergeon',
@@ -34,7 +42,12 @@ const experience = [
   {
     agile: ['Kanban'],
     date: 'February to March 2020',
-    description: 'Being an important player in the creation, coordination and execution of the reutilization plan of the company. Build reusable components for five applications. Support and implement bug fixes and new requirements for each internal application. Coaching new and experienced developers',
+    description: [
+      'Being an important player in the creation, coordination and execution of the reutilization plan of the company.',
+      'Building reusable components for five applications.',
+      'Supporting and implement bug fixes and new requirements for each internal application.',
+      'Coaching new and experienced developers',
+    ],
     subtitle: 'Argentina',
     technologies: ['React', 'Redux', 'Sass', 'BEM', 'AWS', 'Node', 'Npm', 'Storybook'],
     title: 'Web UI Developer at Globant',
@@ -42,7 +55,13 @@ const experience = [
   {
     agile: ['Scrum'],
     date: 'January 2018 to February 2019',
-    description: 'Manager of technical decisions. In charge of the creation and execution of the project\'s deployment plan. Performing code reviews and leading teams to follow coding standards and best practices. Assisted in Web UI Interviews. Evaluating and providing technical/soft skills feedback to team members',
+    description: [
+      'Manager of technical decisions.',
+      'In charge of the creation and execution of the project\'s deployment plan.',
+      'Performing code reviews and leading teams to follow coding standards and best practices.',
+      'Assisted in Web UI Interviews.',
+      'Evaluating and providing technical/soft skills feedback to team members',
+    ],
     subtitle: 'Argentina',
     technologies: ['React', 'Redux', 'Sass', 'BEM', 'Backbone', 'Node', 'Npm', 'Storybook'],
     title: 'Tech Lead at OLX Classifields, Globant',
@@ -50,7 +69,11 @@ const experience = [
   {
     agile: ['Scrum'],
     date: 'April 2017 to January 2018',
-    description: 'Focal point at a multidisciplinary team in one of the biggest accounts at the Latam Region of Globant. Leading front-end features quality and delivery. In charge of the company\'s bootcamp for Web UI Developers',
+    description: [
+      'Focal point at a multidisciplinary team in one of the biggest accounts at the Latam Region of Globant.',
+      'Leading front-end features quality and delivery.',
+      'In charge of the company\'s bootcamp for Web UI Developers'
+    ],
     subtitle: 'Argentina',
     technologies: ['React', 'Redux', 'Sass', 'BEM', 'Backbone', 'Node', 'Npm', 'Storybook'],
     title: 'Focal Web UI Developer at OLX Classifields, Globant',
@@ -58,7 +81,11 @@ const experience = [
   {
     agile: ['Scrum'],
     date: 'October 2015 to April 2017',
-    description: 'Building reusable components for application teams. Eliminating coding and logic errors and ensuring the program perform according to unit test. Performing code reviews and adding automation test with WebDriver / Selenium.',
+    description: [
+      'Building reusable components for application teams.',
+      'Eliminating coding and logic errors and ensuring the program perform according to unit test.',
+      'Performing code reviews and adding automation test with WebDriver / Selenium.',
+    ],
     subtitle: 'Dallas - United States',
     technologies: ['React', 'Redux', 'Sass', 'BEM', 'Npm'],
     title: 'Web UI Developer at Globant',
