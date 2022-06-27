feebeeb9bf2daacaa2565ac1c1fa59e0e58016fc
Maximiliano Tulian <maxi.tulian@nerdery.com>
2022-06-27 09:51:50 -0300

    Update eslint configuration

--- src/app/Experience/Experience.tsx
+++ src/app/Experience/Experience.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import { FaReact } from 'react-icons/fa';
-
 import workExperience from './data';
 import './experience.scss';
 
@@ -10,54 +9,54 @@ interface IWorkItem {
   description: string[] | string;
   date: string;
   technologies: string[];
-};
+}
 
 export default function Experience() {
   return (
     <VerticalTimeline>
-      {workExperience.map(({title, description, date, technologies}: IWorkItem, index: number): ReactNode => {
-        return (
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ borderTop: '4px solid #61dafb'}}
-            iconStyle={{ background: '#61dafb', color: '#fff' }}
-            date={date}
-            icon={<FaReact />}
-            key={index}
-          >
-            <h3 className="vertical-timeline-element-title">{title}</h3>
-            {typeof description === 'string' ? (
-              <p className="experience--paragraph">
-                {description}
-              </p>
-            ) : (
-              <ul className="experience__list">
-                {description.map((descriptionItem: string) =>
-                  <li className="experience__list-item">{descriptionItem}</li>
-                )}
-              </ul>
-            )}
-            {technologies.length > 0 && (
-              <div>
-                <div className="experience__technologies-title">
-                  <strong>Technologies</strong>
-                </div>
-                <div className="experience__technologies-wrapper">
-                  {technologies.length > 0 && (
-                    <>
-                    {technologies.map((technology: string) =>
-                      <div className="experience__tecnologies-item">
-                        {technology}
-                      </div>
-                    )}
-                    </>
-                  )}
-                </div>
-              </div>
-           )}
-          </VerticalTimelineElement>
-        )
-      })}
+      {workExperience.map(({
+        title, description, date, technologies,
+      }: IWorkItem, index: number): ReactNode => (
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ borderTop: '4px solid #61dafb' }}
+          iconStyle={{ background: '#61dafb', color: '#fff' }}
+          date={date}
+          icon={<FaReact />}
+          key={index}
+        >
+          <h3 className="vertical-timeline-element-title">{title}</h3>
+          {typeof description === 'string' ? (
+            <p className="experience--paragraph">
+              {description}
+            </p>
+          ) : (
+            <ul className="experience__list">
+              {description.map((descriptionItem: string, index) => (
+                <li key={index} className="experience__list-item">{descriptionItem}</li>)
+              )}
+            </ul>
+          )}
+          {technologies.length > 0 && (
+          <div>
+            <div className="experience__technologies-title">
+              <strong>Technologies</strong>
+            </div>
+            <div className="experience__technologies-wrapper">
+              {technologies.length > 0 && (
+              <>
+                {technologies.map((technology: string, index) => (
+                  <div className="experience__tecnologies-item" key={index}>
+                    {technology}
+                  </div>
+                ))}
+              </>
+              )}
+            </div>
+          </div>
+          )}
+        </VerticalTimelineElement>
+      ))}
     </VerticalTimeline>
   );
 }
